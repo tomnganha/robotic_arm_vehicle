8643a0c01d572a12b9c3930323b69487b6d01363
unknown <hoangthai050302@gmail.com>
2024-12-11 10:26:12 +0700

    xong tinh nang

---
+++ robotic_arm_vehicle.ino
@@ -0,0 +1,420 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#include <ESP32Servo.h>
+#include <iostream>
+#include <sstream>
+
+int ENA = 1; // PWM control for motor A
+int IN1 = 6;
+int IN2 = 7;
+int IN3 = 8;
+int IN4 = 9;
+int ENB = 10; // PWM control for motor B
+
+int Speed = 255;
+char dieu_khien;
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+#define STOP 0
+
+#define RIGHT_MOTOR 0
+#define LEFT_MOTOR 1
+
+#define FORWARD 1
+#define BACKWARD -1
+
+struct MOTOR_PINS
+{
+  int pinEn;  
+  int pinIN1;
+  int pinIN2;    
+};
+std::vector<MOTOR_PINS> motorPins = 
+{
+  {1, 6, 7}, //RIGHT_MOTOR Pins (EnA, IN1, IN2)
+  {10, 8, 9},  //LEFT_MOTOR  Pins (EnB, IN3, IN4)
+};
+
+struct ServoPins
+{
+  Servo servo;
+  int servoPin;
+  String servoName;
+  int initialPosition;  
+};
+std::vector<ServoPins> servoPins = 
+{
+  { Servo(), 2 , "Base", 90},
+  { Servo(), 3 , "Shoulder", 90},
+  { Servo(), 4 , "Elbow", 90},
+  { Servo(), 5 , "Gripper", 155},
+};
+
+unsigned long previousTimeInMilli = millis();
+
+const char* ssid     = "RobotArm";
+const char* password = "12345678";
+
+AsyncWebServer server(80);
+AsyncWebSocket ArmRobot("/ArmRobot");
+
+const char* htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <style>
+    .arrows {
+      font-size:30px;
+      color:red;
+    }
+    td.button {
+      background-color:black;
+      border-radius:25%;
+      box-shadow: 5px 5px #888888;
+    }
+    td.button:active {
+      transform: translate(5px,5px);
+      box-shadow: none; 
+    }
+
+    .noselect {
+      -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+         -khtml-user-select: none; /* Konqueror HTML */
+           -moz-user-select: none; /* Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently
+                                      supported by Chrome and Opera */
+    }
+
+    .slidecontainer {
+      width: 100%;
+    }
+
+    .slider {
+      -webkit-appearance: none;
+      width: 100%;
+      height: 15px;
+      border-radius: 5px;
+      background: #d3d3d3;
+      outline: none;
+      opacity: 0.7;
+      -webkit-transition: .2s;
+      transition: opacity .2s;
+    }
+
+    .slider:hover {
+      opacity: 1;
+    }
+  
+    .slider::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 25px;
+      height: 25px;
+      border-radius: 50%;
+      background: red;
+      cursor: pointer;
+    }
+
+    .slider::-moz-range-thumb {
+      width: 25px;
+      height: 25px;
+      border-radius: 50%;
+      background: red;
+      cursor: pointer;
+    }
+
+    </style>
+  
+  </head>
+  <body class="noselect" align="center" style="background-color:white">
+    <table id="mainTable" style="width:400px;margin:auto;table-layout:fixed" CELLSPACING=10>
+      <tr>
+        <td></td>
+        <td class="button" ontouchstart='sendButtonInput("MoveCar","1")' ontouchend='sendButtonInput("MoveCar","0")'><span class="arrows" >&#8679;</span></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td class="button" ontouchstart='sendButtonInput("MoveCar","3")' ontouchend='sendButtonInput("MoveCar","0")'><span class="arrows" >&#8678;</span></td>
+        <td class="button"></td>    
+        <td class="button" ontouchstart='sendButtonInput("MoveCar","4")' ontouchend='sendButtonInput("MoveCar","0")'><span class="arrows" >&#8680;</span></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td class="button" ontouchstart='sendButtonInput("MoveCar","2")' ontouchend='sendButtonInput("MoveCar","0")'><span class="arrows" >&#8681;</span></td>
+        <td></td>
+      </tr>
+      <tr/><tr/>
+      <tr>
+        <td style="text-align:left;font-size:25px"><b>Gripper:</b></td>
+        <td colspan=2>
+         <div class="slidecontainer">
+            <input type="range" min="140" max="180" value="155" class="slider" id="Gripper" oninput='sendButtonInput("Gripper",value)'>
+          </div>
+        </td>
+      </tr> 
+      <tr/><tr/>
+      <tr>
+        <td style="text-align:left;font-size:25px"><b>Elbow:</b></td>
+        <td colspan=2>
+         <div class="slidecontainer">
+            <input type="range" min="50" max="120" value="70" class="slider" id="Elbow" oninput='sendButtonInput("Elbow",value)'>
+          </div>
+        </td>
+      </tr> 
+      <tr/><tr/>      
+      <tr>
+        <td style="text-align:left;font-size:25px"><b>Shoulder:</b></td>
+        <td colspan=2>
+         <div class="slidecontainer">
+            <input type="range" min="50" max="150" value="90" class="slider" id="Shoulder" oninput='sendButtonInput("Shoulder",value)'>
+          </div>
+        </td>
+      </tr>  
+      <tr/><tr/>      
+      <tr>
+        <td style="text-align:left;font-size:25px"><b>Base:</b></td>
+        <td colspan=2>
+         <div class="slidecontainer">
+            <input type="range" min="0" max="180" value="90" class="slider" id="Base" oninput='sendButtonInput("Base",value)'>
+          </div>
+        </td>
+      </tr> 
+      <tr/><tr/>       
+    </table>
+  
+    <script>
+      var webSocketArmRobotUrl = "ws:\/\/" + window.location.hostname + "/ArmRobot";
+      var websocketArmRobot;
+      
+      function initArmRobotWebSocket() 
+      {
+        websocketArmRobot = new WebSocket(webSocketArmRobotUrl);
+        websocketArmRobot.onopen    = function(event)
+        {
+          sendButtonInput("Gripper", document.getElementById("Gripper").value);
+          sendButtonInput("Elbow", document.getElementById("Elbow").value);
+          sendButtonInput("Shoulder", document.getElementById("Shoulder").value);
+          sendButtonInput("Base", document.getElementById("Base").value);                    
+        };
+        websocketArmRobot.onclose   = function(event){setTimeout(initArmRobotWebSocket, 2000);};
+        websocketArmRobot.onmessage = function(event){};        
+      }
+      
+      function initWebSocket() 
+      {
+        initArmRobotWebSocket();
+      }
+
+      function sendButtonInput(key, value) 
+      {
+        var data = key + "," + value;
+        websocketArmRobot.send(data);
+        console.log(data);
+      }
+    
+      window.onload = initWebSocket;
+      document.getElementById("mainTable").addEventListener("touchend", function(event){
+        event.preventDefault()
+      });      
+    </script>
+  </body>    
+</html>
+)HTMLHOMEPAGE";
+
+
+void rotateMotor(int motorNumber, int motorDirection)
+{
+  if (motorDirection == FORWARD)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, HIGH);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);    
+  }
+  else if (motorDirection == BACKWARD)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, HIGH);     
+  }
+  else if (motorDirection == STOP)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);       
+  }
+}
+
+void moveCar(int inputValue)
+{
+  switch(inputValue)
+  {
+
+    case UP:
+      rotateMotor(RIGHT_MOTOR, FORWARD);
+      rotateMotor(LEFT_MOTOR, FORWARD);                  
+      break;
+  
+    case DOWN:
+      rotateMotor(RIGHT_MOTOR, BACKWARD);
+      rotateMotor(LEFT_MOTOR, BACKWARD);  
+      break;
+  
+    case LEFT:
+      rotateMotor(RIGHT_MOTOR, FORWARD);
+      rotateMotor(LEFT_MOTOR, BACKWARD);  
+      break;
+  
+    case RIGHT:
+      rotateMotor(RIGHT_MOTOR, BACKWARD);
+      rotateMotor(LEFT_MOTOR, FORWARD); 
+      break;
+ 
+    case STOP:
+      rotateMotor(RIGHT_MOTOR, STOP);
+      rotateMotor(LEFT_MOTOR, STOP);    
+      break;
+  
+    default:
+      rotateMotor(RIGHT_MOTOR, STOP);
+      rotateMotor(LEFT_MOTOR, STOP);    
+      break;
+  }
+}
+
+void handleRoot(AsyncWebServerRequest *request) 
+{
+  request->send_P(200, "text/html", htmlHomePage);
+}
+
+void handleNotFound(AsyncWebServerRequest *request) 
+{
+    request->send(404, "text/plain", "File Not Found");
+}
+
+void sendCurrentRobotArmState()
+{
+  for (int i = 0; i < servoPins.size(); i++)
+  {
+    ArmRobot.textAll(servoPins[i].servoName + "," + servoPins[i].servo.read());
+  }  
+}
+
+void writeServoValues(int servoIndex, int value)
+{
+  servoPins[servoIndex].servo.write(value);   
+}
+
+void onCarInputWebSocketEvent(AsyncWebSocket *server, 
+                      AsyncWebSocketClient *client, 
+                      AwsEventType type,
+                      void *arg, 
+                      uint8_t *data, 
+                      size_t len) 
+{                      
+  switch (type) 
+  {
+    case WS_EVT_CONNECT: {
+      moveCar(0);
+      sendCurrentRobotArmState();
+      break;
+    }
+    case WS_EVT_DISCONNECT: 
+      break;
+
+    case WS_EVT_DATA:
+      AwsFrameInfo *info;
+      info = (AwsFrameInfo*)arg;
+      if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) 
+      {
+        std::string myData = "";
+        myData.assign((char *)data, len);
+        std::istringstream ss(myData);
+        std::string key, value;
+        std::getline(ss, key, ',');
+        std::getline(ss, value, ',');
+        Serial.printf("Key [%s] Value[%s]\n", key.c_str(), value.c_str()); 
+        int valueInt = atoi(value.c_str());     
+        if (key == "MoveCar")
+        {
+          Serial.println("Data received!");
+          moveCar(valueInt);        
+        }
+        else if (key == "Base")
+        {
+          writeServoValues(0, valueInt);            
+        } 
+        else if (key == "Shoulder")
+        {
+          writeServoValues(1, valueInt);           
+        } 
+        else if (key == "Elbow")
+        {
+          writeServoValues(2, valueInt);           
+        }         
+        else if (key == "Gripper")
+        {
+          writeServoValues(3, valueInt);     
+        }             
+      }
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+    default:
+      break;  
+  }
+}
+
+void setUpPinModes()
+{
+      
+  for (int i = 0; i < motorPins.size(); i++)
+  {
+    pinMode(motorPins[i].pinEn, OUTPUT);    
+    pinMode(motorPins[i].pinIN1, OUTPUT);
+    pinMode(motorPins[i].pinIN2, OUTPUT);  
+    /* Attach the PWM Channel to the motor enb Pin */
+    analogWrite(motorPins[i].pinEn, Speed);
+  }
+
+  for (int i = 0; i < servoPins.size(); i++)
+  {
+    servoPins[i].servo.attach(servoPins[i].servoPin);
+    servoPins[i].servo.write(servoPins[i].initialPosition);    
+  }
+
+  moveCar(0);   
+}
+
+
+void setup(void) 
+{
+  setUpPinModes();
+  Serial.begin(9600);
+
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.onNotFound(handleNotFound);
+      
+  ArmRobot.onEvent(onCarInputWebSocketEvent);
+  server.addHandler(&ArmRobot);
+
+  server.begin();
+}
+
+
+void loop() 
+{
+  ArmRobot.cleanupClients(); 
+  vTaskDelay(10 / portTICK_PERIOD_MS);
+}
